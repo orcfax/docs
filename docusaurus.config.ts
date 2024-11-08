207b8d4aa24ad8c83829431490162b3481dbfde7
Gabriel Chartier <gabriel@chartier.link>
2024-11-08 11:31:03 -0500

    install and setup rehype-katex and remark-math plugins

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,8 @@
-import { themes as prismThemes } from "prism-react-renderer";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -35,6 +37,8 @@ const config: Config = {
                 docs: {
                     sidebarPath: "./sidebars.ts",
                     routeBasePath: "/",
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
                 },
                 blog: false,
                 theme: {
@@ -42,6 +46,15 @@ const config: Config = {
                 },
             } satisfies Preset.Options,
         ],
+    ],
+
+    stylesheets: [
+        {
+            href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+            type: "text/css",
+            integrity: "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+            crossorigin: "anonymous",
+        },
     ],
 
     themeConfig: {
