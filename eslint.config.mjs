8734b649072b0093bb8f817e16a76c1b5f2de389
Gabriel Chartier <gabriel@chartier.link>
2024-11-15 11:05:44 -0500

    fix eslint running against generated directories

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,11 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-    { languageOptions: { globals: globals.browser } },
+    { ignores: ["build/**", ".docusaurus/**"] },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        languageOptions: { globals: globals.browser },
+    },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     {
